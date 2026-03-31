d70bb066b4086d8ca88c0ffc287c400b21a9b78e
SPMB Admin <sditinsanrabbani01-maker@users.noreply.github.com>
2026-03-31 21:41:20 +0800

    Update GAS API URL to latest

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycbxcR0EKHA3ZuIE_ev1dn6DVSevY6Y8sOELV_hByI71WKz82_6zeeGrtky3Mik8BcRv-FQ/exec";
+const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycbyybTy6VbeKKAz2ulwcvkyG9ZcjOD2v8LgM93YUgXOM5dz0Ykt4zt4T0QO0HfNCYbMo0A/exec";
 const NAMA_SEKOLAH = "SDIT INSAN RABBANI";
 const ADMIN_USER = "admin"; // Ganti sesukamu
 const ADMIN_PASS = "admin123"; // Ganti sesukamu
