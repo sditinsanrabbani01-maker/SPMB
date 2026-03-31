b7208632dc3e6680a23c42633abefca1f497b91a
sditinsanrabbani01-maker <sditinsanrabbani01@gmail.com>
2026-03-31 11:26:17 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycby3b9GHC-odr0t1LSl6IarxNVnA9HCENbQhRgD9cdVpd0-VqyPo3cj0hk2UxYVEum0o/exec";
+
+// Kamu juga bisa menambah pesan selamat datang global di sini jika perlu
+const SCH_NAME = "SMK/SMA MAJU JAYA";
