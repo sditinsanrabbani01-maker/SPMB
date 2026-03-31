749a9281606fb90672ebbbee776de130959a1670
SPMB Admin <sditinsanrabbani01-maker@users.noreply.github.com>
2026-03-31 15:49:43 +0800

    Add config files

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycbw2VwenY739t0wHncrCLns1dEeb5iCnUopNkhf-nhzsXN9SdrHgqxHa6mTHl5Lnqn_Jeg/exec";
+const NAMA_SEKOLAH = "SDIT INSAN RABBANI";
+const ADMIN_USER = "admin"; // Ganti sesukamu
+const ADMIN_PASS = "admin123"; // Ganti sesukamu
