ccba21691fa0008e07d6ae6b28abe6622e1a863f
SPMB Admin <sditinsanrabbani01-maker@users.noreply.github.com>
2026-03-31 16:08:09 +0800

    Update Google Apps Script API URL to latest deployed version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycby3b9GHC-odr0t1LSl6IarxNVnA9HCENbQhRgD9cdVpd0-VqyPo3cj0hk2UxYVEum0o/exec";
+const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycbxcR0EKHA3ZuIE_ev1dn6DVSevY6Y8sOELV_hByI71WKz82_6zeeGrtky3Mik8BcRv-FQ/exec";
 const NAMA_SEKOLAH = "SDIT INSAN RABBANI";
 const ADMIN_USER = "admin"; // Ganti sesukamu
 const ADMIN_PASS = "admin123"; // Ganti sesukamu
