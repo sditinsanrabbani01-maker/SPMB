162bb4642f220418f30a3f8e823e38f970ce3c0e
sditinsanrabbani01-maker <sditinsanrabbani01@gmail.com>
2026-03-31 11:27:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 const CONFIG_API_URL = "https://script.google.com/macros/s/AKfycby3b9GHC-odr0t1LSl6IarxNVnA9HCENbQhRgD9cdVpd0-VqyPo3cj0hk2UxYVEum0o/exec";
 
 // Kamu juga bisa menambah pesan selamat datang global di sini jika perlu
-const SCH_NAME = "SMK/SMA MAJU JAYA";
+const SCH_NAME = "SDIT INSAN RABBANI";
